a9c2b3f6b6166246577b17aa44f2a88ea6602f94
ongleee <henry49henry.49@gmail.com>
2026-08-21 05:04:02 +0700

    learn print html soup, quote

--- main.py
+++ main.py
@@ -5,15 +5,29 @@ url = "https://quotes.toscrape.com/"
 
 response = requests.get(url)
 
-print(response.status_code)
+# print(response.status_code)
 # print(response.text)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-quote = soup.find("div", class_="quote")
-text = quote.find("span", class_="text")
+quotes = soup.find_all("div", class_="quote")
+print(len(quotes))
 
-print(text.text)
+print("===== QUOTE SCRAPER =====")
+for quote in quotes:
+  tags = quote.find_all("a", class_="tag")
+  tag_list = []
+  for tag in tags:
+    tag_list.append(tag.get_text())
+  text = quote.find("span", class_="text")
+  author = quote.find("small" , class_="author")
 
-author = quote.find("small", class_="author")
-print(author.text)
+  print("Quote: " ,end="")
+  print(text.get_text())
+  print("Author: " ,end="")
+  print(author.get_text())
+  print("Tags: " ,end="")
+  for i in tag_list:
+    print(i,end=' ')
+  print()
+  print()
