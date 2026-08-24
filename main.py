0052ba2b3e0d25f3237fd77c733bbc9cba0358b8
ongleee <henry49henry.49@gmail.com>
2026-08-24 20:46:01 +0700

    learn scrape_page and learn how to scrape all page.

--- main.py
+++ main.py
@@ -1,37 +1,76 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+from urllib.parse import urljoin
 
-url = "https://quotes.toscrape.com/"
+# url = "https://quotes.toscrape.com/"
 
-response = requests.get(url)
-response.raise_for_status()
+
 # print(response.status_code)
 # print(response.text)
 
-soup = BeautifulSoup(response.text, 'html.parser')
 
-quotes = soup.find_all("div", class_="quote")
-print(len(quotes))
 
-quotes_data = []
+
+
+def scrape_page(url):
+  response = requests.get(url)
+  soup = BeautifulSoup(response.text, 'html.parser')
+  response.raise_for_status()
+  quotes = soup.find_all("div", class_="quote")
+  quotes_data = []
+
+  
+  for quote in quotes:
+    tags = quote.find_all("a", class_="tag")
+    tag_list = []
+    for tag in tags:
+      tag_list.append(tag.get_text())
+
+    text = quote.find("span", class_="text")
+    author = quote.find("small" , class_="author")
+
+    data = {
+      "quote": text.get_text(),
+      "author": author.get_text(),
+      "tags": ", ".join(tag_list)
+    }
+
+    quotes_data.append(data)
+
+  nextbutton = soup.find("li", class_="next")
+  if nextbutton:
+    next_a = nextbutton.find("a")
+    next_url = urljoin(url,next_a["href"])
+  else:
+    next_url = None
+
+  
+  return quotes_data,next_url
+
+all_quotes = []
+page = 1
+
+url = "https://quotes.toscrape.com/"
+
+while url:
+  data, next_url = scrape_page(url)
+  all_quotes.extend(data)
+
+  print(f"Scraped: {len(data)} quotes")
+
+  url = next_url
+
+print(len(all_quotes))
 
 print("===== QUOTE SCRAPER =====")
-for quote in quotes:
-  tags = quote.find_all("a", class_="tag")
-  tag_list = []
-  for tag in tags:
-    tag_list.append(tag.get_text())
-  text = quote.find("span", class_="text")
-  author = quote.find("small" , class_="author")
+with open("quotes.csv","w", newline="", encoding="utf-8") as file:
+  fieldnames = ["quote", "author", "tags"]
+  writer = csv.DictWriter(file, fieldnames=fieldnames)
 
+  writer.writeheader()
 
-  data = {
-    "qoute": text.get_text(),
-    "author": author.get_text(),
-    "tags": ", ".join(tag_list)
-  }
-
-  quotes_data.append(data)
+  writer.writerows(all_quotes)
 
 print()
-print(quotes_data)
+print(all_quotes)
